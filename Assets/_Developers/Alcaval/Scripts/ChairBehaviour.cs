b39ab488db918082a1867d6ad4b6a6cf5ae9cda4
Alejandro Camús <alcaval1999@gmail.com>
2022-03-08 22:14:50 +0100

    Changed to script template

---
+++ Assets/_Developers/Alcaval/Scripts/ChairBehaviour.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TFMEsada
+{
+    /// <summary>  
+	/// Brief summary of what the class does
+	/// </summary>
+    public class ChairBehaviour : MonoBehaviour
+    {
+        #region Fields
+      
+        // [Tooltip("Public variables set in the Inspector, should have a Tooltip")]
+        /// <summary>  
+	    /// They should also have a summary
+	    /// </summary>
+        // public static string Ejemplo;
+	  
+	    // private float _ejemplo;
+	  
+	    #endregion
+	  
+	    #region Properties
+	  
+        // public string Ejemplo {get; set;}
+            
+	    #endregion
+	 
+	    #region LifeCycle
+	  
+        // Start, OnAwake, Update, etc
+      
+        #endregion
+
+	    #region Public Methods
+	   
+        #endregion
+
+        #region Private Methods
+	   
+        #endregion
+    }
+}
