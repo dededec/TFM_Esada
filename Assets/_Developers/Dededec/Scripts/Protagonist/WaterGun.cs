2915d5e3ba686def7fccb5a8d797e409ec744616
dededec <daviderenacasado@gmail.com>
2022-03-14 20:57:05 +0100

    Gun mechanics started

--- Assets/_Developers/Dededec/Scripts/Protagonist/WaterGun.cs
+++ Assets/_Developers/Dededec/Scripts/Protagonist/WaterGun.cs
@@ -23,23 +23,47 @@ namespace TFMEsada
         /// </summary>
         [SerializeField] private Movement _movement;
 
+        [Tooltip("Distance for the raycast that dictates if you can shoot an enemy.")]
+        /// <summary>
+        /// Distance for the raycast that dictates if you can shoot an enemy.
+        /// </summary>
+        [SerializeField] private float _aimDistance;
+
+        [Tooltip("LayerMask which determines what objects can be shot.")]
+        /// <summary>
+        /// LayerMask which determines what objects can be shot.
+        /// </summary>
+        [SerializeField] private LayerMask _fireLayer;
+
+
         private InputAction _fire;
 	  
 	    #endregion
 	 
 	    #region LifeCycle
 	
-        // private void OnEnable() 
-        // {
-        //     _fire = _movement.Controls.Player.Fire;
-        //     _fire.performed += fire;
-        //     _fire.Enable();
-        // }
+        private void OnEnable() 
+        {
+            _fire = _movement.Controls.Player.Fire;
+            
+            if(_fire == null)
+            {
+                Debug.LogError("Error: Fire Input Action is null");
+            }
 
-        // private void OnDisable() 
-        // {
-        //     _fire.Disable();
-        // }
+            _fire.performed += fire;
+            _fire.Enable();
+        }
+
+        private void OnDisable() 
+        {
+            _fire.Disable();
+        }
+
+        private void Update() 
+        {
+            Debug.DrawRay(transform.position + Vector3.up, transform.forward * _aimDistance, Color.red);
+        }
       
         #endregion
 
@@ -54,6 +78,26 @@ namespace TFMEsada
             if(!_movement.IsMoving)
             {
                 Debug.Log("Disparaste");
+            }
+
+            /*
+            Creo que lo suyo es que sea en plan autoapuntado (Que no sea un proyectil que se cree),
+            asi que trazamos un raycast palante y ver si está apuntando a un enemigo
+            (tal vez sea mejor trazar un cono, pero esa parte se puede hacer después, es
+            "un módulo aparte").
+            Se traza un raycast, y si choca con algo se dispara (se rota hacia él? tal vez no hace falta).
+            */
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.forward, out hit, _aimDistance, _fireLayer))
+            {
+                /* 
+                Aquí faltarían más cosas, de animación, efectos visuales, sonidos y tal.
+                De hecho tal vez se debería llamar a una función de una clase genérica enemigo,
+                de la cual hereden todos los enemigos por su lado y puedan redefinir la función
+                de muerte o algo por el estilo.
+                */
+                Debug.Log("Disparas a: " + hit.collider.gameObject.name);
+                Destroy(hit.collider.gameObject); 
             }
         }
 	   
