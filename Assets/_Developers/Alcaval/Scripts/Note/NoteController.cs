e70cd05798cd3089536bf1f0632adaeaada6e86b
Alejandro Camús <alcaval1999@gmail.com>
2022-03-23 12:34:32 +0100

    notas almost there

---
+++ Assets/_Developers/Alcaval/Scripts/Note/NoteController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
+
+namespace TFMEsada
+{
+    /// <summary>  
+	/// Brief summary of what the class does
+	/// </summary>
+    public class NoteController : MonoBehaviour
+    {
+        #region Fields
+      
+        private GameObject _speechBubble;
+        [SerializeField] private TMP_Text _text;
+
+        private bool _inReadingRange = false;
+        private bool _control;
+
+        [SerializeField] private Image _pressButtonProp;
+
+
+        [Tooltip("Contents of the note no more than n characters")]
+        [SerializeField] private string[] _contentOfNote;
+        private int _currentDialog = 0;
+
+        [SerializeField] TextWriter tw;
+
+        [SerializeField] private GameObject bubble; 
+	  
+	    #endregion
+
+        public bool Writing{ get; set;}
+	 
+	    #region LifeCycle
+	  
+        private void OnEnable() {
+            _speechBubble = GameObject.FindGameObjectWithTag("Bubble");
+        }
+
+        private void Update() {
+            if(_inReadingRange)
+            {
+                _pressButtonProp.gameObject.SetActive(true);
+                _pressButtonProp.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+                if((Keyboard.current.spaceKey.wasPressedThisFrame || Gamepad.current.buttonSouth.wasPressedThisFrame) && Writing)
+                {
+                    tw.FinishSentence();
+                    Writing = false;
+                }
+                else if((Keyboard.current.spaceKey.wasPressedThisFrame || Gamepad.current.buttonSouth.wasPressedThisFrame) && _currentDialog == _contentOfNote.Length && !Writing)
+                {
+                    _pressButtonProp.gameObject.SetActive(true);
+                    _speechBubble.transform.GetChild(1).GetComponent<TMP_Text>().text = "";
+                    _speechBubble.transform.GetChild(0).gameObject.SetActive(false);
+                    _speechBubble.transform.GetChild(1).gameObject.SetActive(false);
+                    _currentDialog = 0;
+                    _inReadingRange = true;
+                    Writing = false;
+                    GameObject.FindGameObjectWithTag("Player").GetComponent<ControlManager>().TogglePlayerControls(true);
+                }
+                else if((Keyboard.current.spaceKey.wasPressedThisFrame || Gamepad.current.buttonSouth.wasPressedThisFrame) && _currentDialog < _contentOfNote.Length && !Writing)
+                {   
+                    GameObject.FindGameObjectWithTag("Player").GetComponent<ControlManager>().TogglePlayerControls(false);
+                    _pressButtonProp.gameObject.SetActive(true);
+                    _speechBubble.transform.GetChild(0).gameObject.SetActive(true);
+                    _speechBubble.transform.GetChild(1).gameObject.SetActive(true);
+                    tw.AddWriter(_speechBubble.GetComponentInChildren<TMP_Text>(), _contentOfNote[_currentDialog], 0.05f);
+                    Writing = true;
+                    _currentDialog++;
+                }
+
+                RectTransform rt = _speechBubble.transform.GetChild(0).GetComponent<RectTransform>();
+                Vector2 tSize = _text.GetRenderedValues(false);
+                rt.GetComponent<RectTransform>().sizeDelta = new Vector2(rt.GetComponent<RectTransform>().sizeDelta.x, tSize.y + 30);
+
+            }
+        }
+        
+        #endregion
+
+        #region Private Methods
+
+        private void OnTriggerEnter(Collider other) {
+            if(other.tag == "Player")
+            {
+                _inReadingRange = true; 
+            }
+        }
+
+        private void OnTriggerExit(Collider other) {
+            if(other.tag == "Player")
+            {
+                _inReadingRange = false; 
+                _pressButtonProp.gameObject.SetActive(false);
+            }
+        }
+	   
+        #endregion
+    }
+}
--- Assets/_Developers/Alcaval/Scripts/SpeechBublleController.cs
+++ Assets/_Developers/Alcaval/Scripts/SpeechBublleController.cs
@@ -25,6 +25,14 @@ namespace TFMEsada
 	  
         private void Update() {
             transform.position = Camera.main.WorldToScreenPoint(_player.position);
+
+            if(Camera.main.WorldToScreenPoint(_player.position).x > 2 || Camera.main.WorldToScreenPoint(_player.position).x < 5)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }else
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
         }
       
         #endregion
